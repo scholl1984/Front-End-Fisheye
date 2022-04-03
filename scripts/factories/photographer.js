b2ab8c1d2884d8f0b0018ca1fd9c361f604aa945
¨scholl1984¨ <yannick.hervieu@gmail.com>
2022-04-03 20:02:30 +0200

    fin etape 3

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,19 +1,40 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, tagline, price } = data;
     
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/Sample Photos-3/Photographers ID Photos/${portrait}`;
+   
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        const aClick = document.createElement( 'a' );
+        aClick.setAttribute("href", "./photographer.html")
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        img.setAttribute("aria-label", name); 
+        img.style.borderRadius= "50%"; 
+        const h2 = document.createElement( 'a' );
         h2.textContent = name;
-        article.appendChild(img);
+        h2.setAttribute("href", "./photographer.html")
+        const h6 = document.createElement( 'h6' );
+        h6.textContent = city;
+        const tag = document.createElement( 'p' );
+        tag.textContent = tagline;
+        const prix = document.createElement( 'p' );
+        prix.textContent = price;
+        
+        article.appendChild(aClick);
+        aClick.appendChild(img)
         article.appendChild(h2);
+        article.appendChild(h6);
+        article.appendChild(tag);
+        article.appendChild(prix);
+       
         return (article);
+       
     }
-    console.log({name, picture})
+    
+    console.log(data)
     return { name, picture, getUserCardDOM }
 }
+
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,23 @@
-//Mettre le code JavaScript lié à la page photographer.html
+// async function displayData(data) {
+//     const photographersSection = document.querySelector(".photograph-header");
+// console.log(data.photographers)
+//     data.photographers.forEach((photographer) => {
+//         const photographerModel = photographerFactory(photographer);
+//         const userCardDOM = photographerModel.getUserCardDOM();
+//         photographersSection.appendChild(userCardDOM);
+//     });
+// };
+
+// async function init() {
+//     fetch('../data/photographers.json')
+// .then(response => {
+//     return response.json();
+//   }).then(jsondata => {
+//     displayData(jsondata)
+//   }).catch(err => {
+//     // Do something for an error here
+//   });
+   
+// };
+
+// init();
