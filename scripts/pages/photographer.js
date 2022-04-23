29386e77dacc937d75f62735c4e968f1f6164cb7
¨scholl1984¨ <yannick.hervieu@gmail.com>
2022-04-23 22:47:28 +0200

    increment good

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -30,113 +30,92 @@ headerPhotograph.innerHTML = DivHeaderPhotograph;
 
 const [first, last] = name.split(' ');
 
+// Media Photograph
 
 photographerMedia.map((media)=>{
-  let youpi = media.likes;
 
-  if(media.image) {
-    
-  //   function youpi () {
-  //   media.likes.sort();
-  
-  // }
-  // sort () 
-    const section = document.getElementById("photograph-section-id");
+  const section = document.getElementById("photograph-section-id");
+  const linkPictureofthephotograph = document.createElement( 'a' );
+  const pictureofthephotograph = document.createElement( 'img' );
+  const titleofthephotograph = document.createElement( 'div' );
+  const titleofthephoto = document.createElement( 'div' );
+  const  nbrLikes = document.createElement( 'button' );
+
+    if(media.image) { 
+
     // const mediaImage = `assets/photographers/Sample Photos-3/${first}/${media.image}`
     // let mediaPhotographer = `<a><img src="assets/photographers/Sample Photos-3/${first}/${media.image}" class="photophotograph" onclick="displayLighbox()"><div class="title_photo"><div class="title" style="font-size: 13px;">${media.title}</div><div class="nbr_likes">${media.likes} + "&#9829;"</div></div></a>`
-    // console.log(mediaImage)
+    
     // console.log(mediaPhotographer)
     // footer.innerHTML = mediaPhotographer;
 
-    const linkPictureofthephotograph = document.createElement( 'a' );
-    const pictureofthephotograph = document.createElement( 'img' );
-    const titleofthephotograph = document.createElement( 'div' );
+    pictureofthephotograph.setAttribute("src", `assets/photographers/Sample Photos-3/${first}/${media.image}`); 
+    pictureofthephotograph.setAttribute("class", "photophotograph");
+    pictureofthephotograph.setAttribute("onclick", `displayLighbox(${media.id})`);
+
     titleofthephotograph.setAttribute("class", "title_photo");
-    const titleofthephoto = document.createElement( 'div' );
     titleofthephoto.setAttribute("class", "title");
-    const nbrLikes = document.createElement( 'button' );
+
     nbrLikes.setAttribute("class", "nbr_likes");
+    nbrLikes.setAttribute("id", `like-${media.id})`);
+    nbrLikes.innerHTML = media.likes + "" + "&#9829;";
+
     titleofthephotograph.append(titleofthephoto, nbrLikes)
     titleofthephoto.innerHTML = media.title;
     titleofthephoto.style.fontSize = '13px'
-
-
-nbrLikes.addEventListener("click", buttonClick)
-
-let sum = 0;
-for (let i = 0; i < photographerMedia.length; i++)
-sum += photographerMedia[i].likes;
-let imgLikes = media.likes;
-
-    function buttonClick() {
-      imgLikes++;
-      sum++
-        nbrLikes.innerHTML = imgLikes + " " + "&#9829;";
-
-// InfoBar 
-
-let infoBar =  `<div class="nbrTotal_Likes">${sum}</div>
-                  <div class="price_Info">${price}</div>`
-                  
-document.querySelector(".infobar").innerHTML = infoBar;
-    }
-
-    buttonClick()
-
-    pictureofthephotograph.setAttribute("src", `assets/photographers/Sample Photos-3/${first}/${media.image}`); 
-    section.append(linkPictureofthephotograph)
-    pictureofthephotograph.setAttribute("class", "photophotograph");
-    pictureofthephotograph.setAttribute("onclick", `displayLighbox(${media.id})`);
+    
     linkPictureofthephotograph.append(pictureofthephotograph, titleofthephotograph);
+    section.append(linkPictureofthephotograph)
+  
     
   } else if(media.video){
-    const section = document.getElementById("photograph-section-id");
 
     const linkVideoofthephotograph = document.createElement( 'a' );
     const videophotograph = document.createElement( 'video' );
     const titleofthephotograph1 = document.createElement( 'div' );
+    const titleofthevideo = document.createElement( 'div' );
 
     titleofthephotograph1.setAttribute("class", "title_video");
-    const titleofthevideo = document.createElement( 'div' );
     titleofthevideo.setAttribute("class", "title");
-    const nbrLikes = document.createElement( 'button' );
+    
     nbrLikes.setAttribute("class", "nbr_likes");
+    nbrLikes.setAttribute("id", `like-${media.id})`);
+    nbrLikes.innerHTML = media.likes + "" + "&#9829;";
+    
     titleofthephotograph1.append(titleofthevideo, nbrLikes)
     titleofthevideo.innerHTML = media.title;
-
-    nbrLikes.addEventListener("click", buttonClick)
-
-    let sum = 0;
-    for (let i = 0; i < photographerMedia.length; i++)
-    sum += photographerMedia[i].likes;
-    let imgLikes = media.likes;
     
-        function buttonClick() {
-          imgLikes++;
-          sum++
-            nbrLikes.innerHTML = imgLikes + " " + "&#9829;";
-   
-    
-    let infoBar =  `<div class="nbrTotal_Likes">${sum}</div>
-                      <div class="price_Info">${price}</div>`
-                      
-    document.querySelector(".infobar").innerHTML = infoBar;
-        }
-
-    nbrLikes.innerHTML = media.likes + "" + "&#9829;";
     videophotograph.setAttribute("src", `assets/photographers/Sample Photos-3/${first}/${media.video}`);
     videophotograph.setAttribute("type", "video/mp4")
     videophotograph.setAttribute("class", "photophotograph");
     videophotograph.setAttribute("onclick", "displayLighbox()");
+    
     section.append(linkVideoofthephotograph)
     linkVideoofthephotograph.append(videophotograph, titleofthephotograph1);
-  
   }
+
+  let sum = photographerMedia.reduce((acc, media) => {
+    return acc += media.likes
+  }, 0);
+  let infoBar =  `<div id="nbrTotal_Likes">${sum}</div>
+  <div class="price_Info">${price}</div>`
+
+  document.querySelector(".infobar").innerHTML = infoBar;
+  document.getElementById(`like-${media.id})`).addEventListener("click", function (){
+    media.likes++;
+    sum = photographerMedia.reduce((acc, media) => {
+      return acc += media.likes
+    }, 0);
+
+    document.getElementById("nbrTotal_Likes").textContent = sum;
+    document.getElementById(`like-${media.id})`).innerHTML = media.likes + "" + "&#9829;";
+    
+  })
 
 })
 }
 
-// Barre trier 
+// Media Photograph
 
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
@@ -154,6 +133,8 @@ window.onclick = function(event) {
     }
   }
 }
+
+// Function 
 
 async function init() {
     fetch('../data/photographers.json')
